4e8ba7c4595cae2dfb7fa7d675f73b33a750a3b0
AChiu2700 <anna.chiu@csu.fullerton.edu>
2023-04-13 11:19:45 -0700

    Intents on DiscrodBot

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,10 @@
 import discord
 import os
 
-client = discord.Client()
+intents = discord.Intents.default()  # create an Intents object
+intents.members = True  # enable the Members intent, so you can access member events
+
+client = discord.Client(intents=intents)  # create a Client object with the Intents object
 
 @client.event
 async def on_ready():
@@ -15,4 +18,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
+client.run('MTA5NjEyMjIzNDMzNjE5NDY4Mg.G8AOlb.YB5NLzShce_fhevK_dG8u4Fume8YxaTuhb73v0')
