bacad4abe6412e5b0cbbf16fa6f5d29d482d3b2c
AChiu2700 <anna.chiu@csu.fullerton.edu>
2023-04-18 10:31:22 -0700

    Update translation

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+
 import discord
 from discord.ext import commands
 import os
+from googletrans import Translator, constants
+from pprint import pprint
 
 from dotenv import load_dotenv
 
 load_dotenv()
+translator = Translator() # Google Translate API
 
 intents = discord.Intents.default()  # create an Intents object
 intents.members = True  # enable the Members intent, so you can access member events
@@ -15,18 +20,12 @@ client = discord.Client(intents=intents)  # create a Client object with the Inte
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-# async def on_message(message):
-#     if message.content == '$hello':
-#         await message.channel.send('hello')
-
-
 @client.event
 async def on_message(message):
-#     if message.author == client.user: // not the bot ??
-#         return
+    translation = translator.translate(message.content[1:], dest="es") # defaults to english translation
     print(message.content)
     if message.content.startswith('$'):
-        await message.channel.send('Hello!')
+        await message.channel.send(f'{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})')
 
 
 print(os.getenv('TOKEN'))
